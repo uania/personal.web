aeaa12ef99d571bcf69906dbb9a0a9b481931b8b
liuyifan <liuyifan@otw.cn>
2021-02-26 15:35:04 +0800

    添加项目引用

--- src/yx._321/MessageWriter/DefaultAchieve.cs
+++ src/yx._321/MessageWriter/DefaultAchieve.cs
@@ -5,8 +5,9 @@ namespace yx._321.MessageWriter
     {
         private ILogger _logger;
 
-        public DefaultAchieve(ILogger logger){
-_logger = logger;
+        public DefaultAchieve(ILogger logger)
+        {
+            _logger = logger;
         }
         /// <summary>
         /// 记录日志信息
@@ -14,15 +15,16 @@ _logger = logger;
         /// <param name="info"></param>
         public void Info(string info)
         {
-            
+            _logger.LogInformation(info);
         }
 
         /// <summary>
         /// 记录调试信息
         /// </summary>
         /// <param name="info"></param>
-        public void Debug(string info){
-
+        public void Debug(string info)
+        {
+            _logger.LogDebug(info);
         }
     }
 }
